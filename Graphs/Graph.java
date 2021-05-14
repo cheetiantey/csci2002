04fad22318cf21914645e05f3d59e732e657fc05
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-05-14 01:42:26 -0500

    Update Graph.java

--- Graphs/Graph.java
+++ Graphs/Graph.java
@@ -1,4 +1,5 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,37 +11,43 @@ public class Graph {
     @SuppressWarnings("unchecked")
     public Graph(int numVertices) {
         adj = (ArrayList<Integer>[])new Object[numVertices];
+        for (int i = 0; i < numVertices; i++) {
+			adj[i] = new ArrayList<Integer>();
+		}
     }
 
-    void addEdge(int v, int u) {
+    public void addEdge(int v, int u) {
         adj[v].add(u);
         adj[u].add(v);
     }
+
+    public int numVertices() {
+		return adj.length;
+	}
 
     public ArrayList<Integer> adjacent(int v) {
         // Copy constructor 
         return new ArrayList<Integer>(adj[v]);
     }
 
-    // public static Graph readGraph(String filename) {
-    //     FileInputStream inFile = new FileInputStream(filename);
+    public static Graph readGraph(String filename) throws FileNotFoundException {
 
-    //     try(Scanner scan = new Scanner(inFile)) {
-    //         int numVertices = scan.nextInt();
-    //         int numEdges = scan.nextInt();
+		FileInputStream inFile = new FileInputStream(filename);
 
-    //         Graph g = new Graph(numVertices);
-    //         for(int i = 0; i < numEdges; i++) {
-    //             int v = scan.nextInt();
-    //             int u = scan.nextInt();
-    //             g.addEdge(v, u);
-    //         }
-            
-    //         scan.close();
-    //         return g;
-    //     }
-    // }
-    // public static void main(String[] args) {
-
-    // }
+		try (Scanner scan = new Scanner(inFile)) {
+		
+			int numVertices = scan.nextInt();
+			int numEdges    = scan.nextInt();
+			
+			Graph g = new Graph(numVertices);
+			for (int i = 0; i < numEdges; i++) {
+				int v = scan.nextInt();
+				int w = scan.nextInt();
+				g.addEdge(v, w);
+			}
+			
+            scan.close();
+			return g;
+		}
+	}
 }
